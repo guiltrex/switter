4e2b455234b9c5d1ace04a7a96a8a97bfd35bf55
Jiajun <guiltzero@gmail.com>
2012-10-14 00:32:41 -0600

    user sign up model test and model done, there's view form left

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe User do
+	before do
+		@user=User.new(username:'rex', email:'rex@switter.com', password:'rexdbpass', password_confirmation:'rexdbpass')
+	end	
+	subject{ @user }
+	
+	describe "respond to attributes" do
+		it { should respond_to(:username) } 
+		it { should respond_to(:email) }
+		it { should respond_to(:password_digest) }
+		it { should respond_to(:password) }
+		it { should respond_to(:password_confirmation) }				
+	end
+	
+	describe "valid name" do
+		before do
+			@user.username = 'a'*100
+		end	
+		it {should_not be_valid} 
+	end
+	
+	describe "email not blank" do
+		before do
+			@user.email = ' '
+			@user.password = ' ' 
+		end	
+		it {should_not be_valid} 
+	end		
+	
+	describe "email in valid form" do
+		before { @user.email = "foo@FOO" }
+		it {should_not be_valid}
+	end
+	
+	describe "uniqueness in email" do
+		before do
+			user_same_email= @user.dup
+			user_same_email.save
+		end
+		it {should_not be_valid}
+	end
+	
+	describe "passwd minimum 6 digits" do
+		before { @user.password = "rexdb"}
+		it {should_not be_valid}
+	end
+	
+	describe "passwd matches passwd_conf" do
+		before { @user.password_confirmation = 'rexdbmismatch' }
+		it {should_not be_valid}
+	end		
+	
+	describe "passwd_conf nonblanck" do
+		before { @user.password_confirmation = ' ' }
+		it {should_not be_valid}
+	end		
+end
